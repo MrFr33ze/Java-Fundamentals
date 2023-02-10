6c9c62515a0b6bba6c0552ce095d37c69b7a0868
MrFr33ze <122637667+MrFr33ze@users.noreply.github.com>
2023-02-09 20:46:13 -0600

    How to declarate some basic variables
    Int, float, double, char, etc.

---
+++ Java pracrice3/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Is this my first program writing in Java");
+        /*Tipos de variables y declaraciones de las mismas*/
+        int entero = 12;
+        float decimales = 12.23f;
+        double masde2decimales = 3.141592;
+        char caracterunico = 'b';
+        boolean logico = true;
+        String alfabetico = "hola"; // la variable String no es primitiva
+    }
+}
