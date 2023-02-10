f566737630f7f273b15740575534e8651622fcf6
MrFr33ze <122637667+MrFr33ze@users.noreply.github.com>
2023-02-09 20:36:36 -0600

    tipes of documentation basic

---
+++ Java practice1/src/Main.java
@@ -0,0 +1,11 @@
+// Declaracion de la clase main
+public class Main {
+    // Declaracionde del metodo main, void indica que este metodo no revolvera ningun dato al terminar
+    public static void main(String[] args) {
+
+        System.out.println("Esta cadena se imprimira en pantalla y saltara el cursor al proximo reglon");
+        System.out.print("Esta cadena de caracteres se imprimira en pantalla sin salto de reglon");
+
+        System.out.println("Esta\ncadena\nposee\nsaltos\nde\nreglon");// cadena con saltos de reglon en un solo metodo
+    }// Fin del metodo main
+}// Fin de la clase Main
