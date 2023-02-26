dae25fad3c9060eabbb96fba95a7890c2b4a2252
MrFr33ze <122637667+MrFr33ze@users.noreply.github.com>
2023-02-25 22:40:45 -0600

    Syntax of "for" loop
    - Example the aplicating to syntax of FOR loop
    * Iteration count, the number iteration is defined to variable "limite"

---
+++ Java practice7/src/Main.java
@@ -0,0 +1,13 @@
+public class Main { // Declaration public of class Main
+    public static void main(String[] args) {
+
+        System.out.println("This program show the syntax of the 'for' loop");
+        int limite = 10; // variable que limitara iteraciones en el ciclo for
+
+        for (int contador = 0; contador < limite; contador++) // The computer counting from 0 to value of variable limite
+        {
+            System.out.println("Iteration number: " + contador);
+        }
+
+    } // End principal metod main
+} // End the class Main
